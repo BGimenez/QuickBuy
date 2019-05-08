e15827fc24a76123413cf07f9906d39022962683
Bruno Gimenez <bruno.roberto.costa.gimenez@gmail.com>
2019-05-08 15:50:05 -0300

    Criação das Entidades.

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    //abstract - quer dizer que a classe não será instanciada
+    public abstract class Entidade
+    {
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto
+        {
+            get => Id == (int)TipoFormaPagamentoEnum.Boleto;
+        }
+        public bool EhCartaoCredito
+        {
+            get => Id == (int)TipoFormaPagamentoEnum.CartaoCredito;
+        }
+        public bool EhDeposito
+        {
+            get => Id == (int)TipoFormaPagamentoEnum.Deposito;
+        }
+        public bool EhNaoDefinido
+        {
+            get => Id == (int)TipoFormaPagamentoEnum.NaoDefinido;
+        }
+    }
+}
